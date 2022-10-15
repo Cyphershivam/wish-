c9fedd4fc59a2455d03d7d12f5db9fedbff615f8
Cyphershivam <100480356+Cyphershivam@users.noreply.github.com>
2022-10-15 10:41:19 +0530

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,13 @@
+from colorama import Fore , Style
+
+lg = Style.BRIGHT+Fore.LIGHTGREEN_EX
+g = Style.BRIGHT+Fore.GREEN
+lc = Style.BRIGHT+Fore.LIGHTCYAN_EX
+c = Style.BRIGHT+Fore.CYAN
+ly =  Style.BRIGHT+Fore.LIGHTYELLOW_EX
+y = Style.BRIGHT+Fore.YELLOW
+r = Style.BRIGHT+Fore.RED
+lr = Style.BRIGHT+Fore.LIGHTRED_EX
+
+
+                                        
